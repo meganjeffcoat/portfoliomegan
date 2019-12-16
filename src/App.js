65e0f7cd21fca928c554f27c3a1645625def79e2
meganjeffcoat <jeffcoat.megan@gmail.com>
2019-12-16 14:36:08 -0600

    title set up

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
   return (
     <div className="App">
       <Title />
-      <CurrentProjects />
-      <Skills />
+      {/* <CurrentProjects />
+      <Skills /> */}
     </div>
   );
 }
--- src/components/title.js
+++ src/components/title.js
@@ -1,8 +1,44 @@
 import React from 'react';
+import styled from 'styled-components';
+import title from '../assets/images/code.jpg';
+
+const Container = styled.section`
+    width: 99.8%;
+    height: auto;
+    border: 1px solid black;
+    background-image: url(${title});
+    background-size: cover;
+
+`;
+
+const Name = styled.div`
+    margin-top: 30%;
+    margin-left: 20%;
+    margin-right: 20%;
+    color: white;
+    
+    h1 {
+        padding: 3% 0;
+        font-size: 3rem;
+        font-weight: 800;
+        border-top: 3px solid white;
+        border-bottom: 3px solid white;
+    }
+
+    p {
+        font-size: 1.2rem;
+        font-weight: 800;
+    }
+`;
 
 function Title() {
     return (
-        <h1>Title</h1>
+        <Container>
+            <Name>
+             <h1>MEGAN JEFFCOAT</h1> 
+             <p>FULL STACK WEB DEVELOPER</p>  
+            </Name>
+        </Container>
     )
 }
 
