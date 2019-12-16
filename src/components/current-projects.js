9aba18a6b41064e8fa93874d2efeab9ce5433443
meganjeffcoat <jeffcoat.megan@gmail.com>
2019-12-16 14:43:39 -0600

    added text for current projects

--- src/components/current-projects.js
+++ src/components/current-projects.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React from "react";
 
-function CurrentProjects () {
-    return(
-      <h1>Current Projects</h1>  
-    )  
+function CurrentProjects() {
+  return (
+    <section>
+      <h1>Current Projects</h1>
+      <div>
+        <h2>Stampd</h2>
+        <p>
+          Stampd allows you to keep track of your credentials. Your institution
+          will upload the credential where it will be held in the blockchain.
+          You can access your credential with your account and add multiple
+          email addresses that you may have your credentials under.
+        </p>
+      </div>
+      <div>
+        <h2>S&J Architects</h2>
+        <p>
+          A responsive modern architecture website Built with HTML, CSS and ES6
+          JavaScript
+        </p>
+      </div>
+      <div>
+        <h2>Notes</h2>
+        <p>
+          A place to keep notes and the ability to update and remove them Made
+          using React, Redux and Styled Components
+        </p>
+      </div>
+      <div>
+        <h2>Safe Space</h2>
+        <p>
+          Send yourself a positive text and brighten your day Built by a team of
+          five, my focus was on the backend Utilizing express node and twillio
+          for the messaging service
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default CurrentProjects;
